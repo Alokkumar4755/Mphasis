c3c83ee04335e970eff292bfe845ba11462ee3c1
Alokkumar4755 <kumaralok5055@gmail.com>
2019-05-30 06:19:53 -0700

    main class

--- DemoGit/src/Employee.java
+++ DemoGit/src/Employee.java
@@ -1,4 +1,24 @@
 
 public class Employee {
-
+    private int Empid;
+    private String empName;
+    private double empsalary;
+	public int getEmpid() {
+		return Empid;
+	}
+	public void setEmpid(int empid) {
+		Empid = empid;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getEmpsalary() {
+		return empsalary;
+	}
+	public void setEmpsalary(double empsalary) {
+		this.empsalary = empsalary;
+	}
 }
---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,9 @@
+
+public class TestEmployee {
+     public static void main(String[] args) {
+		Employee e1=new Employee();
+		e1.setEmpid(101);
+		e1.setEmpName("alok");
+		e1.setEmpsalary(1000);
+	}
+}
